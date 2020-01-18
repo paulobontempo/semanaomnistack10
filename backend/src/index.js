7e38679c01bdf109db4317bf15bc9e3a0d4f7447
Paulo Bontempo <paulobontempo@gmail.com>
2020-01-18 17:01:32 -0300

    chore(package.json): script start

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,9 @@ const routes = require('./routes');
 
 const app = express();
 
-const mongodbUri = process.env.MONGODB_URI;
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://heroku_wps6lpvx:hfhue79ogobit6f5te6thntreu@ds161001.mlab.com:61001/heroku_wps6lpvx?authSource=heroku_wps6lpvx&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
-if (!mongodbUri) console.log(`mongodb_uri: ${mongodbUri}`);
+// if (!mongodbUri) console.log(`mongodb_uri: ${mongodbUri}`);
 
 mongoose.connect(mongodbUri,
   { useNewUrlParser: true, 
