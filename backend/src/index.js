53acaa0fa2609fe23d121c801cee343749d80342
Paulo Bontempo <paulobontempo@gmail.com>
2020-01-19 00:37:18 -0300

    feat(web/App): addDev

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 const env = require('../config/env');
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect(mongodbUri,
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes); 
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,147 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState, useEffect } from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude ] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      }, {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+  }
+
   return (
     <div id="app">
-      <aside></aside>
-      <main></main>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required value={github_username}  onChange={e => setGithubUsername(e.target.value)} />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required value={techs}  onChange={e => setTechs(e.target.value)} />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="number" name="latitude" id="latitude" required value={latitude} onChange={e => setLatitude(e.target.value)} />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="number" name="longitude" id="longitude" required value={longitude} onChange={e => setLongitude(e.target.value)} />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/8206586?v=4" alt="Paulo Bontempo"/>
+              <div className="user-info">
+                <strong>Paulo Bontempo</strong>
+                <span>JAVA, PHP, Ionic</span>
+              </div>
+            </header>
+            <p>Arquiteto de Software na Mbamobi em tempo integral e criador do life-tally</p>
+            <a href="https://github.com/paulobontempo">Acessar perfil no github</a>
+          </li>
+        </ul>
+      
+      </main>
     </div>
   );
 }
